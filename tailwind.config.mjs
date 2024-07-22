5f31f29d24a59f48e60a04e129118f9b76ad1478
Nico Dinata <nicodnt@icloud.com>
2024-07-22 22:30:54 +1000

    add custom fonts

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,15 @@
+import defaultTheme from "tailwindcss/defaultTheme";
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['"Inter Variable"', ...defaultTheme.fontFamily.sans],
+        serif: ['"Lora Variable"', ...defaultTheme.fontFamily.serif],
+      },
+    },
   },
   plugins: [],
 };
