b1e0892c6f7ca3cb73f50fadcd540341d31b95e5
Nico Dinata <nicodnt@icloud.com>
2024-07-24 23:31:51 +1000

    add inital gh pages deployment config

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://ndinata.github.io",
+  base: "/me",
   integrations: [
     tailwind({
       applyBaseStyles: false,
