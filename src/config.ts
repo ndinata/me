550f172d094d02e6189926467a57cf5930baf16a
Nico Dinata <nicodnt@icloud.com>
2024-07-24 21:19:58 +1000

    add placeholder landing page

---
+++ src/config.ts
@@ -0,0 +1,14 @@
+/** Profile links etc. */
+export const PROFILE = {
+  email: "nicodnt@icloud.com",
+  github: { username: "ndinata", url: "https://github.com/ndinata/" },
+  linkedin: {
+    username: "nicodinata",
+    url: "https://www.linkedin.com/in/nicodinata/",
+  },
+} as const;
+
+/** Website-related config. */
+export const SITE = {
+  year: "2024",
+} as const;
